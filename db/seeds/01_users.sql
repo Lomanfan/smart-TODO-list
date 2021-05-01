c3549eda779b558737591e805eb1d18a94160a20
Liuming <lfan.canada@gmail.com>
2021-05-01 06:05:38 +0000

    inserts values/seeds for users and todo tables

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,27 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- INSERT INTO users (name) VALUES ('Alice');
+-- INSERT INTO users (name) VALUES ('Kira');
+
+INSERT INTO users (id, name, email, password)
+VALUES (1, 'Anne Parks', 'anne@gmail.com', '123'),
+  (2, 'Bobby West', 'bobby@gmail.com', '123'),
+  (3, 'Carl Hart', 'carl@gmail.com', '123')
+  (4, 'Donna Luna', 'donna@gmail.com', '123');
+
+
+
+-- INSERT INTO todo (id, user_id, category_id, user_input, remind_me, completed)
+-- VALUES (1, 1, 1, 'sandles', 2021-05-21, false),
+--   (2, 1, 4, 'Harry Porter', 2021-05-15, false),
+--   (3, 2, 2, 'sushi', 2021-05-13, false),
+--   (4, 2, 2, 'coffee', 2021-05-24, false),
+--   (5, 2, 3, 'Harry Porter', 2021-05-16, false),
+--   (6, 3, 1, 'onesie', 2021-05-10, false),
+--   (7, 3, 4, 'Game of Thrones', 2021-05-11, fasle),
+--   (8, 4, 1, 'naan', 2021-04-03, false);
+
+-- INSERT INTO catagory (id, verb)
+-- VALUES (1, 'buy'),
+--   (2, 'eat'),
+--   (3, 'read'),
+--   (4, 'watch')
