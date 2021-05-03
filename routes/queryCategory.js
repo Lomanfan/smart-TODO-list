13761cf3e506b4f872082338e8fa91400eb96419
Kiup Kim <kukim96@gmail.com>
2021-05-03 12:41:05 -0600

    created queryCategory.js file

---
+++ routes/queryCategory.js
@@ -0,0 +1,39 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'midterm'
+});
+
+const queryCategory = function(user_id, category_id, user_input) {
+  return pool.query(
+  `INSERT INTO todolist (
+    user_id,
+    category_id,
+    user_input
+  )
+  VALUES (
+    $1,
+    $2,
+    $3
+  )
+  RETURNING *;`,[
+    user_id, category_id, user_input
+  ])
+  // INSERT INTO todolist (user_id, category_id, user_input, remind_me_at, is_completed)
+  // VALUES (1, 4, 'sandles', '2021-05-21', FALSE),
+  // INSERT INTO category (type) VALUES ('to shop');
+
+  .then(res => {
+    return res.rows[0];
+  })
+  .catch(err => {
+    return console.log('queryCategory error:', err);
+  });
+};
+exports.queryCategory = queryCategory;
+console.log(queryCategory(1, 1, 'steak'));
+
+
+
