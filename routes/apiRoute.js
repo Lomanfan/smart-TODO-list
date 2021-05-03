e1fee2219d9319f55f56c2835c9982e33900f381
Jessie-p05 <jxpei05@gmail.com>
2021-05-03 03:02:04 +0000

    write a function to get category for the text by API

---
+++ routes/apiRoute.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const express = require('express');
+const router = express.Router();
+const getCategory = require('./getCategory');
+
+
+
+// Use fetch() to POST JSON-encoded data.
+
