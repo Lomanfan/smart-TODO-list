00c8596051397b0fd6329c14fedbf740ecdb675a
Liuming <lfan.canada@gmail.com>
2021-05-05 04:47:18 +0000

    remove spaces, adjustments after group meeting

---
+++ routes/formSubmit.js
@@ -0,0 +1,29 @@
+// $(function () {
+//   //to load all the exist tweet in database
+//   // loadTweets();
+//   //post a tweet after user click submit button
+//   $("#categoryDropdown").onclick(function (event) {
+//     event.preventDefault();
+//     let id = $("#categoryDropdown").data();
+//     console.log(id)
+    // if (textContent.length > 140) {
+    //   $('#error-message-over').slideDown();
+    //   return;
+    // } else if (textContent.length === 0) {
+    //   $('#error-message-empty').slideDown();
+    //   return;
+    // }
+    //send data to server with ajax  and send a get request after
+    // const formData = $(this).serialize();
+    // $.ajax({
+    //   url: '/tweets',
+    //   data: formData,
+    //   type: 'POST'
+    // })
+    // .then(loadTweets)
+    // .then(() => {
+    //   $("#tweet-text").val('');
+    //   $('.counter').html(140);
+    // })
+//   })
+// })
