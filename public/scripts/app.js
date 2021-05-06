d4f0561795bf74b7ab3d902c4004e33fe734fea5
Liuming <lfan.canada@gmail.com>
2021-05-06 22:17:05 +0000

    fixing drop down jQuery

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,20 @@
 //buttons to sort todo-list by category type after DOM is ready
 $(() => {
+  console.log("hiiiiiiiii")
   $(".sortCategory").on('click',function(){
     let sortCate = $(this).text().toLocaleLowerCase();
     if (sortCate ==='all') {
       $(".everyTodo").css( "display", "block" );
     } else {
+      //eat
     sortCate = sortCate.split(' ')[1];
     $(".everyTodo").hide();
+    console.log(sortCate)
+    console.log($(".everyTodo").filter(`.${sortCate}`))
     $(".everyTodo").filter(`.${sortCate}`).css( "display", "block" );
     }
   });
 
 });
+
+
