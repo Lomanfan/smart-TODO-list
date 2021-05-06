2ca3eb6d22a75a4d2545a503761a2d2f321e9c4d
Kiup Kim <kukim96@gmail.com>
2021-05-05 20:51:30 -0600

    solved conflicts

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,14 @@
+//buttons to sort todo-list by category type after DOM is ready
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+  $(".sortCategory").on('click',function(){
+    let sortCate = $(this).text().toLocaleLowerCase();
+    if (sortCate ==='all') {
+      $(".everyTodo").css( "display", "block" );
+    } else {
+    sortCate = sortCate.split(' ')[1];
+    $(".everyTodo").hide();
+    $(".everyTodo").filter(`.${sortCate}`).css( "display", "block" );
     }
-  });;
+  });
+
 });
