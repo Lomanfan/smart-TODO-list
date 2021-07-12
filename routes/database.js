fea9be8106ac5f21c49b3d5f7ea6f88534277ca5
Jessie-p05 <jxpei05@gmail.com>
2021-07-11 23:30:09 -0600

    eslint format

--- routes/database.js
+++ routes/database.js
@@ -1,12 +1,12 @@
 // const todolist = require('./db/seeds/03_todolist.sql');
 // const users = require('./db/seeds/01_users.sql');
-const { __esModule } = require('node-fetch');
-const { Pool } = require('pg');
+const { __esModule } = require("node-fetch");
+const { Pool } = require("pg");
 const pool = new Pool({
-  user: 'labber',
-  password: 'labber',
-  host: 'localhost',
-  database: 'midterm'
+  user: "labber",
+  password: "labber",
+  host: "localhost",
+  database: "midterm",
 });
 
 pool.connect();
@@ -19,11 +19,11 @@ const getToDo = (user) => {
   join users on user_id = users.id
   join category on category_id = category.id
   where users.id = $${queryParams.length}
-  order by todolist.id DESC;`
+  order by todolist.id DESC;`;
   return pool.query(queryString, queryParams).then((data) => {
-  return  data.rows;
-});
-}
+    return data.rows;
+  });
+};
 exports.getTodo = getToDo;
 
 //delete one todo list by its id
@@ -33,12 +33,11 @@ const deleteToDoById = (id) => {
   let queryString = `DELETE
   FROM todolist
   where id = $${queryParams.length}
-  RETURNING *;`
-  return pool.query(queryString, queryParams)
-  .then((res) => {
+  RETURNING *;`;
+  return pool.query(queryString, queryParams).then((res) => {
     return getToDo(res.rows[0]);
   });
-}
+};
 
 exports.deleteToDoById = deleteToDoById;
 
@@ -48,14 +47,12 @@ const changeCateById = (userId, todoId, cateId) => {
   queryParams.push(`${userId}`, `${todoId}`, `${cateId}`);
 
   let queryString = `UPDATE todolist SET category_id = $3
-  WHERE user_id = $1 AND todolist.id = $2 RETURNING *;`
+  WHERE user_id = $1 AND todolist.id = $2 RETURNING *;`;
 
-  return pool.query(queryString, queryParams)
-    .then((res) => {
-      return getToDo(res.rows[0])
-    });
-
-}
+  return pool.query(queryString, queryParams).then((res) => {
+    return getToDo(res.rows[0]);
+  });
+};
 exports.changeCateById = changeCateById;
 
 //get the category id by category type
@@ -64,19 +61,20 @@ const getIdByName = (cateName) => {
   queryParams.push(`${cateName}`);
   let queryString = `SELECT category.id
   FROM category
-  where category.type = $${queryParams.length};`
+  where category.type = $${queryParams.length};`;
   return pool.query(queryString, queryParams).then((data) => {
-  return data.rows;
-});
-}
+    return data.rows;
+  });
+};
 exports.getIdByName = getIdByName;
 
 //get all type in category table
 const getAllCategories = () => {
-  return pool.query(`SELECT * FROM category;`)
-  .then((data) => data.rows)
-  .catch((err) => {
-    console.log("getAllCategories", err.message);
-  })
-}
+  return pool
+    .query(`SELECT * FROM category;`)
+    .then((data) => data.rows)
+    .catch((err) => {
+      console.log("getAllCategories", err.message);
+    });
+};
 exports.getAllCategories = getAllCategories;
